fe2045b3e8fdb485b36a36aed6fc10dd37391791
donna93n <donna.93n@gmail.com>
2014-02-13 18:44:12 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,29 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, addProject);
+
+	console.log("/project/"+idNumber);
+}
+
+function addProject(result){
+	console.log(result);
+	console.log("#"+result.id +" .details");
+
+
+	/*var html ="<img class=\"detailsImage\" src=\""+result.image+"\"+">" 
+		+"<h1>"+result.date+"</h1>"
+		+"<p>"+result.summary+"</p>";*/
+
+	var html = '<a href=# class="thumbnail">' + 
+	 '<img src="' + result['image'] + '"class="detailsImage">' +
+	 '<p>' + result['title'] + '</p>' +
+	 '<p><small>' + result['date'] + '</small></p>' + 
+	 '<p><small>' + result['summary'] + '</small></p></a>';
+	
+	$("#project"+result.id +" .details").html(html);
+
 }
 
 /*
@@ -35,4 +58,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", getColor);
+}
+
+function getColor(result){
+	var colors = result.colors.hex; 
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+	
 }
